67bb773d901b3fb776e26bf4b11ab5ea5fb576fc
netherguy4 <trawe1234@gmail.com>
2025-01-06 15:05:15 +0200

    Enhance header search form styles and transitions for better responsiveness

--- Js/header.js
+++ Js/header.js
@@ -11,7 +11,7 @@ header.addEventListener('click', (e) => {
 		header.querySelector('.header__search-form').classList.add('header__search-form_active');
 		setTimeout(() => {
 			header.querySelector('.header__search-field').focus();
-		}, "300")
+		}, "500")
 		header.querySelector('.header__search-field').addEventListener('blur', restoreNav);
 	}
 	function restoreNav() {
